a77969362c9773f74d7014689a5517025c2a437e
Vallout <valentin.grimm94@gmail.com>
2021-09-11 17:06:54 +0200

    Add homework for week 4

--- code/week04/src/Week04/Homework.hs
+++ code/week04/src/Week04/Homework.hs
@@ -5,17 +5,19 @@
 {-# LANGUAGE TypeApplications  #-}
 {-# LANGUAGE TypeOperators     #-}
 
+{-# OPTIONS_GHC -Wno-deferred-out-of-scope-variables #-}
 module Week04.Homework where
 
 import Data.Aeson            (FromJSON, ToJSON)
 import Data.Functor          (void)
-import Data.Text             (Text)
+import Data.Text             (Text, unpack)
 import GHC.Generics          (Generic)
 import Ledger
 import Ledger.Ada            as Ada
 import Ledger.Constraints    as Constraints
 import Plutus.Contract       as Contract
 import Plutus.Trace.Emulator as Emulator
+import Wallet.Emulator.Wallet
 
 data PayParams = PayParams
     { ppRecipient :: PubKeyHash
@@ -31,11 +33,24 @@ payContract = do
     void $ submitTx tx
     payContract
 
+payContractHandleError :: Contract () PaySchema Text ()
+payContractHandleError = Contract.handleError 
+                (\err -> do 
+                    Contract.logError $ "Here is my error: " ++ unpack err
+                    payContract)
+                payContract
+
 -- A trace that invokes the pay endpoint of payContract on Wallet 1 twice, each time with Wallet 2 as
 -- recipient, but with amounts given by the two arguments. There should be a delay of one slot
 -- after each endpoint call.
 payTrace :: Integer -> Integer -> EmulatorTrace ()
-payTrace _ _ = undefined -- IMPLEMENT ME!
+payTrace a1 a2 = do
+    h1 <- activateContractWallet (Wallet 1) payContractHandleError
+    callEndpoint @"pay" h1 $ PayParams (pubKeyHash $ walletPubKey $ Wallet 2) a1
+    void $ Emulator.waitNSlots 1
+    callEndpoint @"pay" h1 $ PayParams (pubKeyHash $ walletPubKey $ Wallet 2) a2
+    void $ Emulator.waitNSlots 1
+
 
 payTest1 :: IO ()
 payTest1 = runEmulatorTraceIO $ payTrace 1000000 2000000
